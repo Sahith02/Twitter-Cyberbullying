cce424cfca519afc4c020fd2f939d50bc4953f49
Om Shreenidhi <om_719@yahoo.com>
2020-08-14 17:56:54 +0530

    Frontend with Flask

---
+++ flask_backend.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect ,url_for, render_template
+
+app=Flask(__name__)
+
+
+a=[["Rude","You are a bad person"],["Not Rude","Hello , hi!"],["Rude","You suck"]]
+
+l=5
+@app.route("/",methods=["POST", "GET"])
+def home():
+	
+    return render_template("classification.html",content=a)
+
+    
+
+if __name__=="__main__":
+    app.run()
